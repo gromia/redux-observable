b68ac8fc46adfafc78de82f67846ee6fcb28affa
Berkeley Martinez <berkeleytrue@users.noreply.github.com>
2017-06-08 00:31:30 -0700

    fix(epics): Ensure startWith actions are looped back to epics
    Cache actions emitted by epics until an epic init action is dispatched. This init action is dispatch
    synchronously right after the epic is subscribed to. This prevents actions emitted from start of
    epic to hit input subject before action$ is subscribed to.
    
    closes #253

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ export { createEpicMiddleware } from './createEpicMiddleware';
 export { createEpicEnhancer } from './createEpicEnhancer';
 export { ActionsObservable } from './ActionsObservable';
 export { combineEpics } from './combineEpics';
+export { EPIC_INIT } from './EPIC_INIT';
 export { EPIC_END } from './EPIC_END';
 export { ofType } from './operators';
